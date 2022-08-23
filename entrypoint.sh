289df380d9fa362da06d7606a7874fc87df1d623
Mark <markoconnor1@gmail.com>
2022-08-23 15:10:31 +0100

    Typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /blog/tmp/pids/server.pid
 
-# Create database and appply migrations
+# Create database and apply migrations
 rails db:prepare
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
